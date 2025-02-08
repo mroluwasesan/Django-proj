8b4430682c1039ddf3aac43d73a378a465d34164
mroluwasesan <mr.oluwasesan@gmail.com>
2025-02-08 22:13:58 +0100

    update model connction to database

--- app/db/main.py
+++ app/db/main.py
@@ -3,15 +3,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel, create_engine,text
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.books.models import Book
-from app.config import Config
+from app.config import settings
 
 engine = AsyncEngine(
     create_engine(
-        url=Config.DATABASE_URL,
+        url=settings.DATABASE_URL,
         echo=True
         )
     )
 
 async def init_db():
     async with engine.begin() as conn:
+        
         await conn.run_sync(SQLModel.metadata.create_all)
