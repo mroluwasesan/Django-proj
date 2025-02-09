7239008fa2eda38b4915a940d2630f199fa5defd
mroluwasesan <mr.oluwasesan@gmail.com>
2025-02-09 16:54:04 +0100

    create book service

--- app/books/models.py
+++ app/books/models.py
@@ -19,8 +19,8 @@ class Book(SQLModel, table=True):
     published_date: date
     page_count: int
     language: str
-    created_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now()))
-    update_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now()))
+    created_at: datetime = Field(sa_column= Column(pg.TIMESTAMP, default=datetime.now()))
+    update_at: datetime = Field(sa_column= Column(pg.TIMESTAMP, default=datetime.now()))
 
     def __repr__(self):
         return f"<Book({self.title}>"
